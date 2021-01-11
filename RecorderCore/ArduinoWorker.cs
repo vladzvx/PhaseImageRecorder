d77ea702365b56c86226751f3ad4b11919851fb6
vladislav <wladzwer@mail.ru>
2021-01-11 17:34:51 +0300

    arduino test implementation

--- RecorderCore/ArduinoWorker.cs
+++ RecorderCore/ArduinoWorker.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO.Ports;
+using ArduinoDriver;
 
 namespace RecorderCore
 {
     public class ArduinoWorker
     {
-        public void Action(int act)
+        ArduinoDriver.ArduinoDriver driver = new ArduinoDriver.ArduinoDriver(ArduinoUploader.Hardware.ArduinoModel.Mega1284);
+        public void Action(int k)
         {
-            SerialPort serialPort = new SerialPort();
+            for (int i = 2; i <= 10; i++)
+                driver.Send(new ArduinoDriver.SerialProtocol.DigitalWriteRequest((byte)i, DigitalValue.Low));
+
+            driver.Send(new ArduinoDriver.SerialProtocol.DigitalWriteRequest((byte)(k + 2), DigitalValue.High));
         }
     }
 }
