39ce37b6b496fb3d01f36e069ceae63c3c53b811
vladislav <wladzwer@mail.ru>
2021-01-11 18:06:51 +0300

    arduino startup

--- RecorderCore/ArduinoWorker.cs
+++ RecorderCore/ArduinoWorker.cs
@@ -8,9 +8,17 @@ namespace RecorderCore
 {
     public class ArduinoWorker
     {
-        ArduinoDriver.ArduinoDriver driver = new ArduinoDriver.ArduinoDriver(ArduinoUploader.Hardware.ArduinoModel.NanoR3,autoBootstrap:true);
+        ArduinoDriver.ArduinoDriver driver;     
+        private void init()
+        {
+            driver = new ArduinoDriver.ArduinoDriver(ArduinoUploader.Hardware.ArduinoModel.NanoR3, autoBootstrap: true);
+            for (int i = 2; i <= 10; i++)
+                driver.Send(new ArduinoDriver.SerialProtocol.PinModeRequest((byte)i, ArduinoDriver.SerialProtocol.PinMode.Output));
+            driver.Send(new ArduinoDriver.SerialProtocol.DigitalWriteRequest((byte)(8), DigitalValue.High));
+        }
         public void Action(int k)
         {
+            if (driver == null) init();
             for (int i = 2; i <= 10; i++)
                 driver.Send(new ArduinoDriver.SerialProtocol.DigitalWriteRequest((byte)i, DigitalValue.Low));
 
