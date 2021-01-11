62200851b270d0f72721b3cd47a9512520f82810
vladislav <wladzwer@mail.ru>
2021-01-11 19:52:27 +0300

    arduino startup

--- RecorderCore/ArduinoWorker.cs
+++ RecorderCore/ArduinoWorker.cs
@@ -8,21 +8,28 @@ namespace RecorderCore
 {
     public class ArduinoWorker
     {
+        private object locker = new object();
         ArduinoDriver.ArduinoDriver driver;     
         private void init()
         {
-            driver = new ArduinoDriver.ArduinoDriver(ArduinoUploader.Hardware.ArduinoModel.NanoR3, autoBootstrap: true);
-            for (int i = 2; i <= 10; i++)
-                driver.Send(new ArduinoDriver.SerialProtocol.PinModeRequest((byte)i, ArduinoDriver.SerialProtocol.PinMode.Output));
-            driver.Send(new ArduinoDriver.SerialProtocol.DigitalWriteRequest((byte)(8), DigitalValue.High));
+            lock (locker)
+            {
+                driver = new ArduinoDriver.ArduinoDriver(ArduinoUploader.Hardware.ArduinoModel.NanoR3, autoBootstrap: true);
+                for (int i = 2; i <= 10; i++)
+                    driver.Send(new ArduinoDriver.SerialProtocol.PinModeRequest((byte)i, ArduinoDriver.SerialProtocol.PinMode.Output));
+                driver.Send(new ArduinoDriver.SerialProtocol.DigitalWriteRequest((byte)(8), DigitalValue.High));
+            }
+
         }
         public void Action(int k)
         {
             if (driver == null) init();
-            for (int i = 2; i <= 10; i++)
-                driver.Send(new ArduinoDriver.SerialProtocol.DigitalWriteRequest((byte)i, DigitalValue.Low));
-
-            driver.Send(new ArduinoDriver.SerialProtocol.DigitalWriteRequest((byte)(k + 2), DigitalValue.High));
+            lock (locker)
+            {
+                for (int i = 2; i <= 10; i++)
+                    driver.Send(new ArduinoDriver.SerialProtocol.DigitalWriteRequest((byte)i, DigitalValue.Low));
+                driver.Send(new ArduinoDriver.SerialProtocol.DigitalWriteRequest((byte)(k + 2), DigitalValue.High));
+            }
         }
     }
 }
